89fd671c0e586b38b20576ebf9d7ee7bb5aa97d3
Tyxiang <en1434@gmail.com>
2018-04-19 18:35:40 +0800

    调整批处理位置

--- Convert.bat
+++
@@ -1,6 +0,0 @@
-"D:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /icon icon.ico /in Install.ahk
-"D:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /icon icon.ico /in Uninstall.ahk
-
-copy /Y Install.exe ..\Release\OpenWithCMD\
-copy /Y Uninstall.exe ..\Release\OpenWithCMD\
-copy /Y README.md ..\Release\OpenWithCMD\
