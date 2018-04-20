0e958a23deda565b4e7fe37f09dda0e7494c3940
Tyxiang <en1434@163.com>
2018-04-20 19:01:50 +0800

    改变文件结构

--- C.bat
+++
@@ -1,6 +0,0 @@
-"D:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /icon icon.ico /in Install.ahk
-"D:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /icon icon.ico /in Uninstall.ahk
-
-copy /Y Install.exe ..\_release\
-copy /Y Uninstall.exe ..\_release\
-copy /Y README.md ..\_release\
